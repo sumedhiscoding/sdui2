cf122ec446166555812169a2045776ed69e88d52
Sumedh Gavai <sumedh.g@cspl.com>
2024-06-27 13:14:41 +0530

    button

--- frontend/sdui2/src/App.jsx
+++ frontend/sdui2/src/App.jsx
@@ -1,21 +1,28 @@
 
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from "axios"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 import { mapper } from './utils/mapper';
 import * as Components from "react-bootstrap";
 import { mapperComponents } from './utils/mapper';
+import { Field, Form, Formik } from 'formik';
+import FormControl from './components/FormControlModified';
 
 
-const App = () => { 
+ 
+const MyInput = ({ field, form, ...props }) => {
+  return <input {...field} {...props} />;
+};
 
-  const [ui,setUi]=useState(null);
-  const [componentsObjs,setComponentObjs]=useState(null);
-  const [componentsTags,setComponentTags]=useState([]);
-  const [component,setComponent]=useState(null);
-  const [arrayofComps,setArrayofComps]=useState([]);
-  
+const App = () => {
+
+  const [ui, setUi] = useState(null);
+  const [componentsObjs, setComponentObjs] = useState(null);
+  const [componentsTags, setComponentTags] = useState([]);
+  const [component, setComponent] = useState(null);
+  const [arrayofComps, setArrayofComps] = useState([]);
+  const [arrayofButtons,setArrayofButtons]=useState([]);
   // useEffect to call our meta json 
   useEffect(() => {
     const url = "http://localhost:3000"
@@ -33,40 +40,84 @@ const App = () => {
   }, [])
 
 
-  useEffect(()=>{
-    const temp2=ui?.sublayout.components;
-    console.log("temp2",temp2);
+  useEffect(() => {
+    const temp2 = ui?.sublayout.components;
+    console.log("temp2", temp2);
     setComponentObjs(ui?.sublayout.components)
-   
 
 
-  },[ui])
 
-  useEffect(()=>{
-    console.log("componentsObjs",componentsObjs)
-    let temp=componentsObjs?.map((elem,id)=>{
-      console.log("mapper",elem);
-      return mapper(elem.field)(elem.props,elem.children)
+  }, [ui])
+
+
+
+  useEffect(() => {
+    console.log("componentsObjs", componentsObjs)
+    let temp = componentsObjs?.map((elem, id) => {
+      console.log("mapper", elem);
+      if(elem.field!="button")
+      return mapper(elem.field)(elem.props, elem.children)
+    }).filter((elem,id)=>{
+      if(elem!=undefined){
+        return elem
+      }
+    })
+
+    let temp2= componentsObjs?.map((elem,idx)=>{
+      if(elem.field=="button")
+      return mapper(elem.field)(elem.props, elem.children)
+    }).filter((elem)=>{
+      return elem!=undefined
     })
     // console.log("mapper",mapper("input"));
     setArrayofComps(temp);
-    console.log("array of component objects",temp);
+    setArrayofButtons(temp2);
+    console.log("array of component objects", temp);
+    console.log("array of temp2 ", temp2)
+  }, [componentsObjs])
 
-  },[componentsObjs])
-
-  useEffect(()=>{
-    console.log("Array of components",arrayofComps);
-  },[arrayofComps])
+  useEffect(() => {
+    console.log("Array of components", arrayofComps);
+    console.log("Array of Buttons",arrayofButtons);
+  }, [arrayofComps,arrayofButtons])
 
   return (
     <div>
-      {/* {mapperComponents[0].tag}
-      {mapperComponents[2].tag} */}
-      
-     {arrayofComps && arrayofComps.map((elem,id)=>{
-      return (<React.Fragment key={id}>{elem}</React.Fragment>)
-     })}
 
+      <Formik
+        initialValues={{ email: '',email2:'' }}
+        onSubmit={(values, actions) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            actions.setSubmitting(false);
+          }, 1000);
+        }}
+      >
+        {(props) => (
+          <Form>
+            {/* {arrayofComps && arrayofComps.map((elem, id) => {
+              // if(elem.)
+              return (<Field key={id} component={elem}/>)
+            })} */}
+            {/* <Field name="email" placeholder="Doe" component={FormControl}  /> */}
+
+            {
+            //  arrayofComps && arrayofComps.map((elem)={
+            //   return (<Field name /> );
+            //  })
+            }
+          {arrayofComps &&
+            // <Field name="email" placeholder="DOe" component={arrayofComps[0]}/>
+            <Field name="email2" placeholder="DOe1"  component={MyInput} />
+          }  
+                {
+                  arrayofComps &&
+                  // console.log("myINput",MyInput, typeof(MyInput))
+                  console.log("arrayofComp",arrayofComps[0],typeof(arrayofComps[0]))
+                }
+          </Form>)}
+
+      </Formik>
 
     </div>
   )
--- frontend/sdui2/src/utils/mapper.jsx
+++ frontend/sdui2/src/utils/mapper.jsx
@@ -20,7 +20,7 @@ export const mapperComponents = [
 
 
 export const mapper = (name) => {
-    console.log("name", name);
+    console.log("name", name,typeof(name));
     const elem = mapperComponents.find((elem, id) => {
         return elem.name == String(name);
     })
